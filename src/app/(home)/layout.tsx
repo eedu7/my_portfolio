90efab85b88c7ffcc35fb41b2f7341952b73fecf
eedu7 <mueedahmad067@gmail.com>
2024-12-24 15:20:32 +0500

    FEAT: Added background

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import DotPattern from "@/components/ui/dot-pattern";
+import {cn} from "@/lib/utils";
 
 
 const Layout = ({children}: Readonly<{children: React.ReactNode}>) => {
     return (
         <>
             {/* Header */}
-            <header>
-                <p>Header</p>
+            <header className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
+                <p className="z-10 whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-black dark:text-white">Mueed Ahmad</p>
+                <DotPattern
+                    className={cn(
+                        "[mask-image:radial-gradient(300px_circle_at_center,white,transparent)]",
+                    )}
+                />
             </header>
 
             {/* Main body */}
