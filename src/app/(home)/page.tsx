6fc1fe662d34132e252708bd436bc3017e1ac1bc
eedu7 <mueedahmad067@gmail.com>
2025-01-03 16:03:47 +0500

    Modified: The font style and title

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,24 +5,27 @@ import Image from "next/image";
 import HeroSectionImage from "@/constants/images";
 
 export default function Home() {
-  return (
-      <section
-          className="relative h-[500px] overflow-hidden rounded-lg bg-background md:shadow-xl mt-24 md:mt-0">
-          <div className="w-full z-10 h-full md:grid grid-cols-2 content-center max-w-screen-lg mx-auto sm:mt-28 md:mt-0">
-              <div className="col-span-1 flex flex-col justify-center items-center">
-                  <p className="z-10 whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-black dark:text-white">Mueed
-                      Ahmad</p>
-              </div>
-              <div className="grid-cols-1 w-full h-full flex items-center justify-center">
-                  <Image src={HeroSectionImage} alt='Hero Section Image' className="lg:size-80 md:size-64 size-44" />
-              </div>
-          </div>
+    return (<section
+            className="relative h-[500px] overflow-hidden rounded-lg bg-background md:shadow-xl">
+            <div
+                className="w-full z-10 h-full md:grid grid-cols-2 content-center max-w-screen-lg mx-auto mt-24 md:mt-0">
+                <div className="col-span-1 flex flex-col justify-center items-center mx-4">
+                        <h1 className="z-10 whitespace-pre-wrap text-left text-xl w-full font-bold tracking-wide font-mono text-gray-500 dark:text-white">Hi
+                            !</h1>
+                        <h1 className="z-10 whitespace-pre-wrap text-left text-4xl w-full font-extrabold tracking-wide text-titleColor font-mono dark:text-white">I'm
+                            Mueed</h1>
+                        <h1 className="z-10 whitespace-pre-wrap text-xl text-left w-full font-bold font-mono tracking-wide text-gray-400 dark:text-white">I'm
+                            a <span className="text-gray-500">Full Stack</span> Developer</h1>
+                        <p className="z-10 whitespace-pre-wrap text-left text-lg w-full font-mono tracking-wide mt-6 text-gray-400 dark:text-white">I specialized in developing scalable backend services.</p>
+                </div>
+                <div className="col-span-1 w-full h-full flex items-center justify-center pb-40 md:pb-0">
+                    <Image src={HeroSectionImage} alt='Hero Section Image'
+                           className="lg:size-80 md:size-64 size-44 z-10"/>
+                </div>
+            </div>
 
-          <DotPattern
-              className={cn(
-                  "[mask-image:radial-gradient(300px_circle_at_center,white,transparent)]",
-              )}
-          />
-      </section>
-  );
+            <DotPattern
+                className={cn("[mask-image:radial-gradient(300px_circle_at_center,white,transparent)]",)}
+            />
+        </section>);
 }
